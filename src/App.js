163b696ee45122d4a0121278443a76e26ac4dcc4
Henry Lay <henry88lay@gmail.com>
2019-07-14 17:32:33 +1000

    Constructed two Components
    Constructed Search Component and SearchButton Component to clean out the app.js file. tested the button and working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Shelves from './components/Shelves';
+import Search from './components/Search';
 // import * as BooksAPI from './BooksAPI'
 import './App.css';
+import SearchButton from './components/SearchButton';
 
 class BooksApp extends React.Component {
   state = {
@@ -14,35 +16,16 @@ class BooksApp extends React.Component {
     showSearchPage: false
   };
 
+  updateSearchPageState = state => {
+    this.setState({showSearchPage: state});
+  };
+
   render() {
     return (
       <div className='app'>
         {this.state.showSearchPage ? (
-          // Search Component
-          <div className='search-books'>
-            <div className='search-books-bar'>
-              <button
-                className='close-search'
-                onClick={() => this.setState({showSearchPage: false})}
-              >
-                Close
-              </button>
-              <div className='search-books-input-wrapper'>
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type='text' placeholder='Search by title or author' />
-              </div>
-            </div>
-            <div className='search-books-results'>
-              <ol className='books-grid' />
-            </div>
-          </div>
+          /* Search Component */
+          <Search showSearchPage={this.updateSearchPageState} />
         ) : (
           <div className='list-books'>
             {/* Header Component */}
@@ -52,11 +35,7 @@ class BooksApp extends React.Component {
             {/* Shelves Component */}
             <Shelves />
             {/* Search Button Component */}
-            <div className='open-search'>
-              <button onClick={() => this.setState({showSearchPage: true})}>
-                Add a book
-              </button>
-            </div>
+            <SearchButton showSearchPage={this.updateSearchPageState} />
           </div>
         )}
       </div>
