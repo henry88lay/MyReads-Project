e5930963f43ace40611fe30d9322190026e33213
Henry Lay <henry88lay@gmail.com>
2019-07-14 21:03:59 +1000

    refactoed Shelf.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import Shelves from './components/Shelves';
 import Search from './components/Search';
 import * as BooksAPI from './BooksAPI';
 import SearchButton from './components/SearchButton';
 import Header from './components/Header';
 import './App.css';
+
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
-    books: [],
-    query: ''
+    allBooks: [],
+    filteredBooks: []
   };
 
   updateSearchPageState = state => {
@@ -38,18 +33,20 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className='app'>
-        {this.state.showSearchPage ? (
-          <Search showSearchPage={this.updateSearchPageState} />
-        ) : (
-          <div className='list-books'>
-            <Header />
-            <Shelves
-              allBooks={this.state.books}
-              changeShelf={this.changeBookShelf}
-            />
-            <SearchButton showSearchPage={this.updateSearchPageState} />
-          </div>
-        )}
+        <Switch>
+          {this.state.showSearchPage ? (
+            <Search showSearchPage={this.updateSearchPageState} />
+          ) : (
+            <div className='list-books'>
+              <Header />
+              <Shelves
+                allBooks={this.state.books}
+                changeShelf={this.changeBookShelf}
+              />
+              <SearchButton showSearchPage={this.updateSearchPageState} />
+            </div>
+          )}
+        </Switch>
       </div>
     );
   }
--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -2,52 +2,48 @@ import React, {Component} from 'react';
 
 export default class Shelf extends Component {
   render() {
-    const shelfBooks = this.props.books;
+    const {book, changeOption} = this.props;
+    let shelfVal = book.shelf ? book.shelf : 'move';
 
     return (
-      <div className='bookshelf'>
-        <h2 className='bookshelf-title'>{this.props.title}</h2>
-        <div className='bookshelf-books'>
-          <ol className='books-grid'>
-            {shelfBooks.map(book => (
-              <li key={book.id}>
-                <div className='book'>
-                  <div className='book-top'>
-                    <div
-                      className='book-cover'
-                      style={{
-                        width: 128,
-                        height: 193,
-                        backgroundImage: `url(${book.imageLinks.thumbnail})`
-                      }}
-                    />
-                    <div className='book-shelf-changer'>
-                      <select
-                        value={book.shelf}
-                        onChange={e =>
-                          this.props.changeShelf(book, e.target.value)
-                        }
-                      >
-                        <option value='move' disabled>
-                          Move to...
-                        </option>
-                        <option value='currentlyReading'>
-                          Currently Reading
-                        </option>
-                        <option value='wantToRead'>Want to Read</option>
-                        <option value='read'>Read</option>
-                        <option value='none'>None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className='book-title'>book.title</div>
-                  <div className='book-authors'>book.authors</div>
-                </div>
-              </li>
+      <li>
+        <div className='book'>
+          <div className='book-top'>
+            {book.imageLinks && (
+              <div
+                className='book-cover'
+                style={{
+                  width: 128,
+                  height: 193,
+                  backgroundImage: `url(${book.imageLinks.thumbnail})`
+                }}
+              />
+            )}
+
+            <div className='book-shelf-changer'>
+              <select
+                value={shelfVal}
+                onChange={e => changeOption(book, e.target.value)}
+              >
+                <option value='move' disabled>
+                  Move to...
+                </option>
+                <option value='currentlyReading'>Currently Reading</option>
+                <option value='wantToRead'>Want to Read</option>
+                <option value='read'>Read</option>
+                <option value='none'>None</option>
+              </select>
+            </div>
+          </div>
+          <div className='book-title'>{book.title}</div>
+          {book.author &&
+            book.authors.map((author, index) => (
+              <div key={index} className='book-authors'>
+                {author}
+              </div>
             ))}
-          </ol>
         </div>
-      </div>
+      </li>
     );
   }
 }
